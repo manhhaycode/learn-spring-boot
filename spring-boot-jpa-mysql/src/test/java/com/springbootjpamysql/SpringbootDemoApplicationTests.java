acb44869b4d57b1c69ff9f28c74aea54dd460b0b
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-11-23 15:16:16 +0700

    add dockerize, dockercompose mysql

--- spring-boot-jpa-mysql/src/test/java/com/springbootjpamysql/SpringbootDemoApplicationTests.java
+++ spring-boot-jpa-mysql/src/test/java/com/springbootjpamysql/SpringbootDemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.springbootjpamysql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootDemoApplicationTests.class)
 class SpringbootDemoApplicationTests {
 
 	@Test
