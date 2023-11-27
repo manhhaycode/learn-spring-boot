38ef37dafa9de187eb9f2e8263662f26c387e185
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-11-27 08:25:59 +0700

    fix bug ci lombok project

--- springboot-jpa-lombok/src/test/java/com/springbootjpalombok/SpringbootJpaLombokApplicationTests.java
+++ springboot-jpa-lombok/src/test/java/com/springbootjpalombok/SpringbootJpaLombokApplicationTests.java
@@ -3,7 +3,7 @@ package com.springbootjpalombok;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootJpaLombokApplicationTests.class)
 class SpringbootJpaLombokApplicationTests {
 
 	@Test
